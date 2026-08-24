06d019fc89ecd3a0d66bb0d5bcf6b4ee3bab535d
AnushkaKarle03 <anushkakarle03@gmail.com>
2026-08-24 18:55:09 +0530

    Add files via upload

---
+++ sqroot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n=0;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    float sqt=sqrt(n);
+    printf("%f",sqt);
+    return 0;
+}
