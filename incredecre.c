b282c87399a5384078e58f28f520d083fc51ee54
AnushkaKarle03 <anushkakarle03@gmail.com>
2026-08-30 11:36:07 +0530

    Add files via upload

---
+++ incredecre.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the a:");
+    scanf("%d",&a);
+    a++;
+    printf("a=%d\n",a);
+    a--;
+    printf("a=%d\n",a);
+    return 0;
+}
